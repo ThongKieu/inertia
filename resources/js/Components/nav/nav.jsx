655606b52743fcc90c79075be6188062d1f0ea5a
{ThongKieu} <{kieuthong16dth@gmail.com}>
2023-08-04 16:39:58 +0700

    update

--- resources/js/Components/nav/nav.jsx
+++ resources/js/Components/nav/nav.jsx
@@ -2,27 +2,25 @@ import React from "react";
 import {
     Navbar,
     Collapse,
-    Typography,
     Button,
     Menu,
-    MenuHandler,
     MenuList,
     MenuItem,
     Avatar,
     IconButton,
 } from "@material-tailwind/react";
 import {
-    CubeTransparentIcon,
+    HomeIcon,
     UserCircleIcon,
-    CodeBracketSquareIcon,
-    Square3Stack3DIcon,
+
+
     ChevronDownIcon,
     PowerIcon,
     Bars2Icon,
 } from "@heroicons/react/24/outline";
 import CardMain from "./card";
 import NavLink from '@/Components/NavLink';
-import Dropdown from '@/Components/Dropdown';
+import ApplicationLogo from "../ApplicationLogo";
 // import NavGuest from "./navGuest";
 
 // profile menu component
@@ -45,29 +43,24 @@ function ProfileMenu() {
     return (
         <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
             <div className="relative flex items-center mx-auto text-blue-gray-900">
-                <a
-                    to="/"
+                <NavLink
+                    href={route('dashboard')}
                     className="py-1 ml-2 font-medium cursor-pointer"
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-green-500">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
                     </svg>
-
-
-                </a>
-                <a
-                    to="/"
+                </NavLink>
+                <NavLink
+                    href={route('dashboard')}
                     className="ml-2 cursor-pointer py-1.5 font-medium "
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-blue-500">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z" />
                     </svg>
-
-
-
-                </a>
-                <a
-                    to="/"
+                </NavLink>
+                <NavLink
+                    href={route('dashboard')}
                     className="ml-2 cursor-pointer py-1.5 font-medium "
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-yellow-500">
@@ -75,15 +68,15 @@ function ProfileMenu() {
                     </svg>
 
 
-                </a>
-                <a
-                    to="/"
+                </NavLink>
+                <NavLink
+                    href={route('dashboard')}
                     className="mr-4 ml-2 cursor-pointer py-1.5 font-medium "
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-red-500">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
                     </svg>
-                </a>
+                </NavLink>
                 <Button
                     variant="text"
                     color="blue-gray"
@@ -109,8 +102,8 @@ function ProfileMenu() {
                 {profileMenuItems.map(({ label, icon, href }, key) => {
                     const isLastItem = key === profileMenuItems.length - 1;
                     return (
-                        <a key={key}
-                            to={href}
+                        <NavLink key={key}
+                            href={href}
                             className={`flex items-center gap-2 rounded outline-none ${isLastItem
                                 ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
                                 : ""
@@ -122,7 +115,7 @@ function ProfileMenu() {
                                 className: `h-4 w-4 ${isLastItem ? "text-red-500" : ""}`,
                                 strokeWidth: 2,
                             })}
-                        </a>
+                        </NavLink>
                     );
                 })}
             </MenuList>
@@ -130,68 +123,12 @@ function ProfileMenu() {
     );
 }
 
-// nav list menu
-// const navListMenuItems = [
-//     {
-//         title: "VỊ TRÍ",
-//         href: "/"
-
-//     },
-//     {
-//         title: "DANH SÁCH THỢ",
-//         href: "/"
-//     },
-
-// ];
-
-// function NavListMenu() {
-//     const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
-//     const renderItems = navListMenuItems.map(({ title, href }) => (
-//         <a to={href} key={title}>
-//             <Typography variant="h6" color="blue-gray" className="mb-1">
-//                 {title}
-//             </Typography>
-//         </a>
-//     ));
-
-//     return (
-//         <React.Fragment>
-//             <Menu allowHover open={isMenuOpen} handler={setIsMenuOpen} >
-//                 <MenuHandler>
-//                     <Typography as="a" href="#" variant="small" className="font-normal">
-//                         <MenuItem className="items-center hidden gap-2 text-blue-gray-900 lg:flex lg:rounded-full">
-//                             <Square3Stack3DIcon className="h-[18px] w-[18px]" /> Thông Tin Thợ {" "}
-//                             <ChevronDownIcon
-//                                 strokeWidth={2}
-//                                 className={`h-3 w-3 transition-transform ${isMenuOpen ? "rotate-180" : ""
-//                                     }`}
-//                             />
-//                         </MenuItem>
-//                     </Typography>
-//                 </MenuHandler>
-//                 <MenuList className="hidden w-[10rem] gap-3 overflow-visible lg:grid">
-//                     <ul className="flex flex-col w-full gap-1 ">
-//                         {renderItems}
-//                     </ul>
-//                 </MenuList>
-//             </Menu>
-//             <MenuItem className="flex items-center gap-2 text-blue-gray-900 lg:hidden">
-//                 <Square3Stack3DIcon className="h-[18px] w-[18px]" /> Thông Tin Thợ {" "}
-//             </MenuItem>
-//             <ul className="flex flex-col w-full gap-1 ml-6 lg:hidden">
-//                 {renderItems}
-//             </ul>
-//         </React.Fragment>
-//     );
-// }
-
 // nav list component
 const navListItems = [
 
     {
         id: 1, label: "Trang Chủ",
-        icon: CodeBracketSquareIcon,
+        icon: HomeIcon,
         href: '/'
     },
     {
@@ -217,7 +154,7 @@ function NavList() {
 
             {navListItems.map(({ label, icon }, key) => (
                 <NavLink key={key} href={route('dashboard')} className="font-normal">
-                    <MenuItem className="flex items-center gap-2 lg:rounded-full">
+                    <MenuItem className="flex items-center gap-2 text-black lg:rounded-full">
                         {React.createElement(icon, { className: "h-[18px] w-[18px]" })}{" "}
                         {label}
                     </MenuItem>
@@ -252,12 +189,12 @@ function NavbarDefault() {
                     <Bars2Icon className="w-6 h-6" />
                 </IconButton>
                 <div className="hidden p-0 m-0 lg:flex">
-                    <a
-                        to="/"
+                    <NavLink
+                        href={route('dashboard')}
                         className="mr-4 ml-2 cursor-pointer py-1.5 font-medium "
                     >
-                        <img className="w-8 h-8" src="https://thoviet.com.vn/wp-content/uploads/2023/03/cropped-Logo-Tho-Viet-bg-3.png" alt="LogoTV" />
-                    </a>
+                        <ApplicationLogo/>
+                    </NavLink>
                     <NavList />
                 </div>
                 <CardMain />
@@ -265,13 +202,10 @@ function NavbarDefault() {
                     <ProfileMenu />
                 </div>
             </div>
-
-
             <Collapse open={isNavOpen} className="overflow-scroll ">
                 <CardMain />
                 <NavList />
             </Collapse>
-
         </Navbar>
     );
 }
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,22 +1,393 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 import FloatingButton from '@/Components/nav/floatingButton';
+import React, { useState, useEffect } from 'react'
+import { Button, Card } from "@material-tailwind/react";
+import {
+    TrashIcon, PaintBrushIcon
 
+} from "@heroicons/react/24/outline";
+const TABLE_HEAD = ["Yêu Cầu Công Việc", "Địa Chỉ", "Quận", "Số Điện Thoại", "Thợ", "Chức Năng"];
+const TABLE_HEAD_RIGHT = ["Nội Dung Công Việc", "BH", "Địa Chỉ KH", "KV",  "Thanh Toán","SDT", "KTV", "Chi","Thu", "Số Phiếu Thu",  "Chức Năng"];
+var dataNew = [{
+    idCV: "",
+    yccv: "",
+    diaChi: "",
+    quan: "",
+    sdt: "",
+},]
+const data = [
+    {
+        idCV: 1,
+        yccv: "Sửa Nhà",
+        diaChi: "Trần Hưng Đạo",
+        quan: "q1",
+        sdt: "0947613923",
+        BH: '1 t',
+        dsThu: '1.500.000đ',
+        dsChi: '500.000đ',
+        soPhieuThu: '17.1717',
+        tinhTrangTT: 'chua tt'
+    },
+    {
+        idCV: 2,
+        yccv: "Sửa Máy Lạnh",
+        diaChi: "15 Nguyễn Trãi",
+        quan: "q1",
+        sdt: "0947613923", BH: '1 t',
+        dsThu: '1.500.000đ',
+        dsChi: '500.000đ',
+        soPhieuThu: '17.1717',
+        tinhTrangTT: 'chua tt'
+    },
+    {
+        idCV: 3,
+        yccv: "Sửa Điện",
+        diaChi: "30 Trần Xuân Soạn",
+        quan: "q7",
+        sdt: "0947613923", BH: '1 t',
+        dsThu: '1.500.000đ',
+        dsChi: '500.000đ',
+        soPhieuThu: '17.1717',
+        tinhTrangTT: 'chua tt'
+    },
+    {
+        idCV: 4,
+        yccv: "Sửa máy bớm nước",
+        diaChi: "15 Phạm Thế Hiển",
+        quan: "q8",
+        sdt: "0947613923", BH: '1 t',
+        dsThu: '1.500.000đ',
+        dsChi: '500.000đ',
+        soPhieuThu: '17.1717',
+        tinhTrangTT: 'chua tt'
+    },
+    {
+        idCV: 5,
+        yccv: "Sửa dốc dắt xe",
+        diaChi: "456 Sư Vạn Hạnh",
+        quan: "q10",
+        sdt: "0947613923", BH: '1 t',
+        dsThu: '1.500.000đ',
+        dsChi: '500.000đ',
+        soPhieuThu: '17.1717',
+        tinhTrangTT: 'chua tt'
+    },
+    {
+        idCV: 6,
+        yccv: "",
+        diaChi: "",
+        quan: "",
+        sdt: "", BH: '1 t',
+        dsThu: '1.500.000đ',
+        dsChi: '500.000đ',
+        soPhieuThu: '17.1717',
+        tinhTrangTT: 'chua tt'
+    },
+];
+const listWorker = [
+    {
+        idTho: 1,
+        maNV: "A03",
+        tenNV: "Có",
+        hoVaDem: "Nguyen Xuan",
+        linhVuc: "DN"
+
+    },
+    {
+        idTho: 2,
+        maNV: "A04",
+        tenNV: "Tiền",
+        hoVaDem: "Nguyen Xuan",
+        linhVuc: "DN"
+
+    }, {
+        idTho: 3,
+        maNV: "A05",
+        tenNV: "Nhạc",
+        hoVaDem: "Nguyen Xuan",
+        linhVuc: "DN"
+
+    },
+    {
+        idTho: 4,
+        maNV: "A06",
+        tenNV: "Thông",
+        hoVaDem: "Nguyen Xuan",
+        linhVuc: "DL"
+
+    },
+    {
+        idTho: 5,
+        maNV: "A07",
+        tenNV: "Thống",
+        hoVaDem: "Nguyen Xuan",
+        linhVuc: "XD"
+
+    }
+]
 export default function Dashboard({ auth }) {
+    const [workData, setWorkData] = useState(data)
+    const [worksData, setWorksData] = useState(dataNew)
+    const onChangeInput = (e, idCV) => {
+        const { name, value } = e.target
+        console.log('name', name)
+        console.log('value', value)
+        console.log('idCV', idCV)
+        const editData = workData.map((item) =>
+            item.idCV === idCV && name ? { ...item, [name]: value } : item
+        )
+        setWorkData(editData)
+    }
+    const [selectedOption, setSelectedOption] = useState('');
+    const [options, setOptions] = useState([]);
+    useEffect(() => {
+        setOptions(listWorker);
+    }, []);
+
+    const handleOptionChange = (event) => {
+        setSelectedOption(event.target.value);
+    };
+    const [screenSize, setScreenSize] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight - 100,
+    });
+    var heightScreenTV = screenSize.height;
+    console.log('kich thuoc', heightScreenTV);
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="text-xl font-semibold leading-tight text-gray-800">Dashboard</h2>}
         >
-            <Head title="Dashboard" />
+            <Head title="Trang Chủ" />
+            <Card className={`h-[${heightScreenTV}px] grid w-full  grid-flow-col overflow-scroll auto-cols-max mt-1`}>
+                {/* bang ben trai  */}
+                <table className="w-full text-left border-r-4 border-red-500 table-auto min-w-max">
+                    <thead>
+                        <tr>
+                            {TABLE_HEAD.map((head) => (
 
-            <div className="py-12">
-                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
-                    </div>
-                </div>
-            </div>
+                                <th key={head} className="p-1 text-sm font-normal leading-none border-b opacity-70 border-blue-gray-100 bg-blue-gray-50 w-fit">
+                                    {head}
+                                </th>
+                            ))}
+                        </tr>
+
+                    </thead>
+                    <tbody>
+                        {workData.map(({ yccv, diaChi, sdt, quan, idCV }, index) => {
+                            const isLast = index === data.length - 1;
+                            const classes = isLast ? "w-fit " : "border-b border-blue-gray-50 w-fit";
+                            const classGeneral1 = "border text-black p-1 rounded border-blue-gray-50 bg-white shadow-lg shadow-blue-gray-900/5 ring-4 ring-transparent placeholder:text-blue-gray-200 focus:!border-blue-500 focus:!border-t-blue-500 focus:ring-blue-500/20 outline-none "
+                            return (
+                                // <input className='w-full' value={workData} />
+                                <tr key={idCV}>
+                                    <td className={classes}>
+                                        <input
+                                            name='yccv'
+                                            value={yccv}
+                                            type="text"
+                                            onChange={(e) => onChangeInput(e, idCV)}
+                                            placeholder="Yêu Cầu Công Việc"
+                                            className={classGeneral1}
+
+                                        />
+                                    </td>
+                                    <td className={`${classes} bg-blue-gray-50/50`}>
+                                        <input
+                                            name='diaChi'
+                                            type="text"
+                                            placeholder="Địa Chỉ"
+                                            className={classGeneral1}
+                                            value={diaChi}
+                                            onChange={(e) => onChangeInput(e, idCV)}
+                                        />
+                                    </td>
+                                    <td className={classes}>
+
+                                        <input
+                                            name='quan'
+                                            type="text"
+                                            placeholder="Quận"
+                                            className={`${classGeneral1} text-center w-12`}
+                                            value={quan}
+                                            onChange={(e) => onChangeInput(e, idCV)}
+                                        />
+                                    </td>
+                                    <td className={classes}>
+                                        <input
+                                            name='sdt'
+                                            type="text"
+                                            placeholder="Số Điện Thoại"
+                                            className={`${classGeneral1} w-28 text-center`}
+                                            value={sdt}
+                                            onChange={(e) => onChangeInput(e, idCV)}
+                                        />
+                                    </td>
+                                    <td className={`${classes} bg-blue-gray-50/50 w-20`}>
+                                        <select id="selectOption" value={selectedOption} onChange={handleOptionChange} className={classGeneral1}>
+                                            <option value="">Chọn</option>
+                                            {options.map((option) => (
+                                                <option key={option.tenNV} value={option.tenNV}>
+                                                    {option.tenNV}
+                                                </option>
+                                            ))}
+                                        </select>
+
+                                    </td>
+                                    <td className={`w-32 ${classes} `}>
+                                        <Button variant="outlined" className='p-1 mr-1 text-red-500 border-red-500 border-none'><TrashIcon className='w-4 h-4' /> </Button>
+                                        <Button variant="outlined" className='p-1 text-blue-500 border-blue-500 border-none '><PaintBrushIcon className='w-4 h-4' /></Button>
+                                    </td>
+
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+                {/* bang ben phai */}
+                <table className="w-full text-left table-auto min-w-max ">
+                    <thead>
+                        <tr>
+                            {TABLE_HEAD_RIGHT.map((head) => (
+                                <th key={head} className="p-1 text-sm font-normal leading-none border-b opacity-70 border-blue-gray-100 bg-blue-gray-50">
+                                    {head}
+                                </th>
+                            ))}
+                        </tr>
+
+                    </thead>
+                    <tbody>
+                        {workData.map(({ yccv, diaChi, sdt, quan, idCV, dsChi, dsThu, BH, tinhTrangTT, soPhieuThu }, index) => {
+                            const isLast = index === data.length - 1;
+                            const classes = isLast ? "p-1 w-3 " : "p-1 border-b border-blue-gray-50 w-3";
+                            const classGeneral = "border  p-1 rounded border-blue-gray-50 bg-white text-black shadow-lg shadow-blue-gray-900/5 ring-4 ring-transparent placeholder:text-blue-gray-200 focus:!border-blue-500 focus:!border-t-blue-500 focus:ring-blue-500/20 outline-none "
+                            return (
+                                <tr key={idCV}>
+
+                                    <td className={classes}>
+                                        <input
+                                            name='yccv'
+                                            value={yccv}
+                                            type="text"
+                                            onChange={(e) => onChangeInput(e, idCV)}
+                                            placeholder="Nội Dung Công Việc"
+                                            className={classGeneral}
+
+                                        />
+                                    </td>
+                                    <td className={classes}>
+
+                                        <input
+                                            name='quan'
+                                            type="text"
+                                            placeholder="BH"
+                                            className={`${classGeneral} text-center w-12`}
+                                            value={BH}
+                                            onChange={(e) => onChangeInput(e, idCV)}
+                                        />
+                                    </td>
+                                    <td className={`${classes} bg-blue-gray-50/50`}>
+                                        <input
+                                            name='diaChi'
+                                            type="text"
+                                            placeholder="Địa Chỉ"
+                                            className={classGeneral}
+
+                                            value={diaChi}
+                                            onChange={(e) => onChangeInput(e, idCV)}
+                                        />
+                                    </td>
+
+                                    <td className={classes}>
+
+                                        <input
+                                            name='quan'
+                                            type="text"
+                                            placeholder="Quận"
+                                            className={`${classGeneral} text-center w-12`}
+                                            value={quan}
+                                            onChange={(e) => onChangeInput(e, idCV)}
+                                        />
+                                    </td>
+                                    <td className={classes}>
+
+                                        <input
+                                            name='quan'
+                                            type="text"
+                                            placeholder=""
+                                            className={`${classGeneral} text-center w-16`}
+                                            value={tinhTrangTT}
+                                            onChange={(e) => onChangeInput(e, idCV)}
+                                        />
+                                    </td>
+                                    <td className={classes}>
+                                        <input
+                                            name='sdt'
+                                            type="text"
+                                            placeholder="Số Điện Thoại"
+                                            className={`${classGeneral} w-28 text-center`}
+                                            value={sdt}
+                                            onChange={(e) => onChangeInput(e, idCV)}
+                                        />
+                                    </td>
+                                    <td className={`${classes} bg-blue-gray-50/50 w-20`}>
+                                        {/* <p>{listNV.map((itemNV)=>{
+                                            <p  value={itemNV.idTho}>{itemNV.tenNV}</p>
+                                            console.log('nhan vien:',itemNV.tenNV);
+                                        })}</p> */}
+                                        <select id="selectOption" value={selectedOption} onChange={handleOptionChange} className={classGeneral}>
+                                            <option value="">Chọn</option>
+                                            {options.map((option) => (
+                                                <option key={option.tenNV} value={option.tenNV}>
+                                                    {option.tenNV}
+                                                </option>
+                                            ))}
+                                        </select>
+
+                                    </td>
+                                    <td className={classes}>
+                                        <input
+                                            name='sdt'
+                                            type="text"
+                                            placeholder="Chi"
+                                            className={`${classGeneral} w-28 text-center`}
+                                            value={dsChi}
+                                            onChange={(e) => onChangeInput(e, idCV)}
+                                        />
+                                    </td>
+                                    <td className={classes}>
+                                        <input
+                                            name='sdt'
+                                            type="text"
+                                            placeholder="Thu"
+                                            className={`${classGeneral} w-28 text-center`}
+                                            value={dsThu}
+                                            onChange={(e) => onChangeInput(e, idCV)}
+                                        />
+                                    </td>
+                                    <td className={classes}>
+                                        <input
+                                            name='sdt'
+                                            type="text"
+                                            placeholder="Thu"
+                                            className={`${classGeneral} w-28 text-center`}
+                                            value={soPhieuThu}
+                                            onChange={(e) => onChangeInput(e, idCV)}
+                                        />
+                                    </td>
+
+                                    <td className={`w-32 ${classes} `}>
+                                        <Button variant="outlined" className='p-1 mr-1 text-red-500 border-red-500 border-none'><TrashIcon className='w-4 h-4' /> </Button>
+                                        <Button variant="outlined" className='p-1 text-blue-500 border-blue-500 border-none '><PaintBrushIcon className='w-4 h-4' /></Button>
+
+
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </Card>
             <div className='fixed bottom-2 right-2'>
                 <FloatingButton />
             </div>
